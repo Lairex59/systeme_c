7b64f80970b01734f2137fef4a821d4416382b47
Lairex59 <laraibahmed2004@gmail.com>
2021-04-19 15:49:27 +0200

    malloc

--- hammingcode/test.c
+++ hammingcode/test.c
@@ -2,7 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-	FILE *pfad;
-	if (pfad = fopen("Code.txt", 
+/**
+ * 
+ * 
+ * 
+ */
+int main(char *vor[10]) {
+	FILE *pfad, *out;
+	char byte[9];
+	char code[12];
+
+	if ((pfad = fopen("code.txt","w")) != NULL) {
+		
+		fprintf(pfad, "\nhelo\n");
+		for(int i = 0; i < 10; i++) {
+			if (*vor[i] == 0 || *vor[i] == 1) { 
+				fprintf(pfad, "%i", &vor[i]);
+			}
+		}	
+		
+		fclose(pfad);
+	}
 }
